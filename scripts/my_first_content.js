90e483a24bbd65900231efb79511b85fe10b78d8
zhouhuajian <zhouhuajian@company.com>
2024-05-24 13:48:43 +0800

    添加代码

---
+++ scripts/my_first_content.js
@@ -0,0 +1,5 @@
+alert("我的第一个扩展");
+let userIpt = document.querySelector("#user_login");
+let pwdIpt = document.querySelector("#user_password");
+userIpt.value = "zhouhuajian@company.com";
+pwdIpt.value = "mypassword";
